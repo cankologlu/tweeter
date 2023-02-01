ca4200b4a42b59f0b48ba20b8f7dbe988a1aa82a
Can Kologlu <mckologlu@gmail.com>
2023-02-01 18:20:41 -0500

    implemented the rendertweets function to loop through the array

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -19,7 +19,7 @@ const tweetData = {
 }
 
 const createTweetElement = (tweet) => {
-  const tweetElement = `<article class="article-tweet">
+  const $tweetElement = `<article class="article-tweet">
   <header class="article-tweet-header">
     <div class="header-user">
       <img width="50px" class="header-user-avatar" src=${tweet.user.avatars}>
@@ -35,7 +35,7 @@ const createTweetElement = (tweet) => {
       </p>
     </div>
   <footer class="twitter-footer">
-    <span class="tweet-date">10 days ago</span>
+    <span class="tweet-date">${tweet.created_at}</span>
     <div class="tweet-symbols"> 
     <div class="flag"><i class="fa-solid fa-flag"></i></div>
     <div class="heart"><i class="fa-solid fa-heart"></i></div>
@@ -43,14 +43,53 @@ const createTweetElement = (tweet) => {
     </div>
   </footer>
 </article>`
-return tweetElement;
+return $tweetElement;
 }
-$(document).ready(function () {
-  const $tweet = createTweetElement(tweetData);
 
-  // Test / driver code (temporary)
-  console.log($tweet); // to see what it looks like
-  $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
+
+/* Event Listener*/
+
+const renderTweets = (tweetsArray) => {
+  for(let tweetObj of tweetsArray) {
+   let $tweet = createTweetElement(tweetObj);
+   $('#tweets-container').append($tweet);
+  } 
+}
+
+
+
+
+
+$(document).ready(function () {
+  // const $tweet = createTweetElement(tweetData);
+  renderTweets(data)
+ 
+ 
+  // to add it to the page so we can make sure it's got all the right elements, classes, etc.
 });
 
 
