f148887503a1b5186c6784f4a302605fbae5d437
Can Kologlu <mckologlu@gmail.com>
2023-02-03 01:22:37 -0500

    added comments to function

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,15 +1,15 @@
 // const { redirect } = require("express/lib/response");
 
-$(document).ready(function() {
-  const counter = $(".counter")
-  $( ".new-tweet form textarea" ).on("input",function() {
+$(document).ready(function() {       // Updates counter with .on jquery function
+  const counter = $(".counter");
+  $(".new-tweet form textarea").on("input",function() {
     const limit = 140;
-    const currentLength = this.value.length
+    const currentLength = this.value.length;
     counter.text(limit - currentLength);
-    if (currentLength > 140 ) {
-        counter.addClass("max-char-limit"); 
+    if (currentLength > 140) {
+      counter.addClass("max-char-limit");
     } else {
-      counter.removeClass("max-char-limit")
+      counter.removeClass("max-char-limit");
     }
   });
 });
